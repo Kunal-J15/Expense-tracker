7f21aa633f8c3de5fdb4afd8f6002186006e0b95
KJ <subodhsjagtap19@gmail.com>
2023-01-04 15:03:04 +0530

    login and axios in frontend

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -2,8 +2,14 @@ const User = require("../models/user");
 
 
 exports.addUser = async(req,res,next)=>{
+    try {
     const {name,email,password} = req.body;
     const user = User.build({name,email,password});
     await user.save();
     res.send("succefuly saved")
+    } catch (error) {
+        // console.log(error);
+        res.status(404).send("user already exist")
+    }
+    
 }
---
+++ frontEnd/signup-login.js
@@ -0,0 +1,45 @@
+const base = "http://localhost:3000/user/";
+
+const signIn = document.querySelector("#signIn");
+const logIn = document.querySelector("#logIn");
+const resDiv = document.querySelector("#res");
+if(signIn){
+signIn.addEventListener("submit",async (e)=>{
+    e.preventDefault();
+    const data = new FormData(signIn);
+    let obj ={};
+    for (const [name,value] of data) {
+        console.log(name,value);
+      obj[name] = value;
+      console.log(obj);
+    }
+    try {
+        const res = await axios.post(base,obj);
+        giveFeed(res.data);
+    } catch (error) {
+        giveFeed(error.response.data);
+    }
+        
+})
+}
+logIn.addEventListener("submit",async(e)=>{
+    e.preventDefault();
+    const data = new FormData(logIn);
+    let obj ={};
+    for (const [name,value] of data) {
+        console.log(name,value);
+      obj[name] = value;
+    }
+    try {
+        const res = await axios.post(base,obj);
+        giveFeed(res.data);
+    } catch (error) {
+        giveFeed(error.response.data);
+    }
+})
+function giveFeed(msg) {
+    resDiv.innerText = msg;
+    setTimeout(() => {
+        resDiv.innerText=""
+    }, 2000);
+}
